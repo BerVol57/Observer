e0a0f99b1dd9007d9f880f7256e453775938bed9
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-04-19 23:30:58 +0300

    Lab commit

---
+++ main.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class EventManager(ABC):
+    @abstractmethod
+    def subscribe(self, listener: Listener) -> None:
+        pass
+
+    @abstractmethod
+    def unsubscribe(self, listener: Listener) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+
+
+class Editor(EventManager):
+    _state: str = None
+    _listeners: List[Listener] = []
+    f = None
+    file = None
+    k = 0
+
+    def subscribe(self, listener: Listener) -> None:
+        print("Editor: Listener is subscribed")
+        self._listeners.append(listener)
+
+    def unsubscribe(self, listener: Listener) -> None:
+        print("Editor: Listener is subscribed")
+        self._listeners.remove(listener)
+
+    def notify(self) -> None:
+        print("Editor: Notifying listeners...")
+        for listener in self._listeners:
+            listener.update(self)
+
+    def plus(self) -> None:
+        self.k += 1
+        self._state = "plus"
+        self.notify()
+
+    def minus(self) -> None:
+        self.k -= 1
+        self._state = "minus"
+        self.notify()
+
+
+class Listener(ABC):
+    @abstractmethod
+    def update(self, event: EventManager) -> None:
+        pass
+
+
+class ListenerA(Listener):
+    plus: int = None
+
+    def update(self, event: EventManager) -> None:
+        print("ListenerA has been notified")
+        if event._state == "plus":
+            self.plus = event.k
+
+
+class ListenerB(Listener):
+    minus: int = None
+
+    def update(self, event: EventManager) -> None:
+        print("ListenerB has been notified")
+        if event._state == "minus":
+            self.minus = event.k
+
+
+if __name__ == "__main__":
+    _editor = Editor()
+
+    listener_a = ListenerA()
+    _editor.subscribe(listener_a)
+
+    listener_b = ListenerB()
+    _editor.subscribe(listener_b)
+
+    _editor.plus()
+    _editor.minus()
+
+    _editor.unsubscribe(listener_a)
+
+    _editor.minus()
---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+from main import *
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_editor_action(self):
+        _editor = Editor()
+        k_save = _editor.k
+        _editor.plus()
+        self.assertEqual(_editor.k, k_save + 1)
+        _editor.minus()
+        self.assertEqual(_editor.k, k_save)
+        _editor.minus()
+        self.assertEqual(_editor.k, k_save - 1)
+
+    def test_notify(self):
+        _editor = Editor()
+
+        _listener_a = ListenerA()  # +
+        _editor.subscribe(_listener_a)
+
+        _listener_b = ListenerB()  # -
+        _editor.subscribe(_listener_b)
+
+        _editor.plus()
+        self.assertEqual(_editor.k, _listener_a.plus)
+        _editor.minus()
+        self.assertEqual(_editor.k, _listener_b.minus)
+
+
+if __name__ == '__main__':
+    unittest.main()
